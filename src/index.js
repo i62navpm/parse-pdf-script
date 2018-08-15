a8b04a268850cf50186c827a034a4c028f33f444
i62navpm <manuelnavarro1987@gmail.com>
2018-08-15 13:40:45 +0200

    Prepare like a package

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 require('module-alias/register')
 const fs = require('fs')
 const Parser = require('@src/Parser')
-const pdfFiles = require('@config/filesConfig')
+const debug = require('debug')
+const error = debug('app:error')
+const log = debug('app:log')
 
-const outputFile = 'pdfFiles'
+async function main(
+  pdfFiles = require('@config/filesConfig'),
+  send2File = true
+) {
+  const outputFile = 'pdfFiles'
+  function save2Json(value) {
+    const fileRegex = /.*\/(\D+)\.\D{3}/gm
+    value.map((data, index) => {
+      let filename = pdfFiles[index].replace(fileRegex, `${outputFile}/$1.json`)
 
-function save2Json(value) {
-  const fileRegex = /.*\/(\D+)\.\D{3}/gm
-  value.map((data, index) => {
-    let filename = pdfFiles[index].replace(fileRegex, `${outputFile}/$1.json`)
+      fs.writeFile(filename, JSON.stringify(data), err => {
+        if (err) {
+          return error(err)
+        }
 
-    fs.writeFile(filename, JSON.stringify(data), err => {
-      if (err) {
-        return console.log(err)
-      }
-
-      console.log(`\nThe file: "${filename}" was saved!\n`)
+        log(`The file: "${filename}" was saved!`)
+      })
     })
+  }
+  let promises = pdfFiles.map(file => {
+    log(`Parsing the file: "${file}"`)
+    return new Parser(file)
   })
-}
-let promises = pdfFiles.map(file => {
-  console.log(`\nParsing the file: "${file}"\n`)
-  return new Parser(file)
-})
 
-Promise.all(promises).then(save2Json)
+  let result = await Promise.all(promises)
+
+  send2File && save2Json(result)
+
+  return result
+}
+
+module.exports = main
